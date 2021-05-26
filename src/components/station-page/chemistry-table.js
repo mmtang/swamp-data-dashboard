665d315b7e7dd8e1dd5943261081090adcd174cb
Michelle Tang <msmetang@gmail.com>
2021-05-25 22:22:12 -0700

    feat: added column sort and pagination to base data table component

--- src/components/station-page/chemistry-table.js
+++ src/components/station-page/chemistry-table.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import DataTable from './data-table';
+import { timeParse } from 'd3-time-format';
 
 
 export default function ChemistryTable(props) {
@@ -9,6 +10,10 @@ export default function ChemistryTable(props) {
         if (props.station) {
             getData()
             .then((records) => {
+                const parseDate = timeParse('%Y-%m-%d')
+                records.forEach(d => {
+                    d.resultWithUnit = d.Result.toString() + ' ' + d.Unit
+                });
                 console.log(records);
                 setData(records);
             });
@@ -19,19 +24,23 @@ export default function ChemistryTable(props) {
         return [
             {
                 Header: 'Analyte',
-                accessor: 'Analyte'
+                accessor: 'Analyte',
+                sortType: 'string'
             },
             {
                 Header: 'Last Sample Date',
-                accessor: 'SampleDate'
+                accessor: 'SampleDate',
+                sortType: 'string'
             },
             {
                 Header: 'Last Sample Result',
-                accessor: 'Result'
-            },
+                accessor: 'resultWithUnit',
+                disableSortBy: true
+            }
         ]
     }, [])
 
+    
     const getData = () => {
         return new Promise((resolve, reject) => {
             let url = 'https://data.ca.gov/api/3/action/datastore_search?resource_id=555ee3bf-891f-4ac4-a1fc-c8855cf70e7e&limit=100';
@@ -46,6 +55,8 @@ export default function ChemistryTable(props) {
     }
     
     return (
-        <DataTable columns={columns} data={data} />
+        <div style={{ marginBottom: '100px' }}>
+            <DataTable columns={columns} data={data} />
+        </div>
     )
 }
--- src/components/station-page/data-table.js
+++ src/components/station-page/data-table.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTable } from 'react-table'
+import { useTable, useSortBy, usePagination } from 'react-table'
 
 
 export default function DataTable({ columns, data }) {
@@ -7,39 +7,100 @@ export default function DataTable({ columns, data }) {
         getTableProps,
         getTableBodyProps,
         headerGroups,
-        rows,
+        //rows,
         prepareRow,
-    } = useTable({ columns, data })
+        page,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize }
+    } = useTable(
+        { 
+            columns, 
+            data 
+        }, 
+        useSortBy,
+        usePagination
+    )
 
     return (
-        <table {...getTableProps()} style={{ width: '100%', border: '1px solid #e3e4e6'}}>
-            <thead>
-                {headerGroups.map(headerGroup => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map(column => (
-                            <th {...column.getHeaderProps()} style={{ background: '#fff', fontWeight: 'bold' }}>
-                                {column.render('Header')}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {rows.map(row => {
-                    prepareRow(row)
-                    return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map(cell => {
-                                return (
-                                    <td {...cell.getCellProps()} style={{ padding: '10px', border: 'solid 1px #e3e4e6'}}>
-                                        {cell.render('Cell')}
-                                    </td>
-                                )
-                            })}
+        <React.Fragment>
+            <table {...getTableProps()} style={{ width: '100%' }}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())} style={{ background: '#fff', fontWeight: 'bold' }}>
+                                    {column.render('Header')}
+                                    <span>
+                                        {column.isSorted ? (column.isSortedDesc ? '<' : '>') : ''}
+                                    </span>
+                                </th>
+                            ))}
                         </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {page.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return (
+                                        <td {...cell.getCellProps()} style={{ padding: '10px', border: 'solid 1px #e3e4e6'}}>
+                                            {cell.render('Cell')}
+                                        </td>
+                                    )
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            <div className="pagination">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
+                <span>
+                    Page{' '}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{' '}
+                </span>
+                <span>
+                    | Go to page:{' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                        const page = e.target.value ? Number(e.target.value) - 1 : 0
+                        gotoPage(page)
+                        }}
+                        style={{ width: '100px' }}
+                    />
+                </span>{' '}
+                <select value={pageSize} onChange={e => { setPageSize(Number(e.target.value)) }}>
+                    {[10, 20, 50].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </React.Fragment>
     )
 }
