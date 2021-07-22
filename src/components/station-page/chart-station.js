a8c26180e323c22890091593ec5f8d9493b00e6b
Michelle Tang <msmetang@gmail.com>
2021-07-21 17:17:46 -0700

    feat: working graph and modal for graphing multiple parameters on station page

--- src/components/station-page/chart-station.js
+++ src/components/station-page/chart-station.js
@@ -1,17 +1,201 @@
 import React, { useState, useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-//import CustomTooltip from './custom-tooltip';
-import { timeParse, timeFormat } from 'd3';
+import { event as currentEvent } from 'd3-selection';
 import { Button, Header, Icon, Modal } from 'semantic-ui-react';
 import { colorPaletteViz } from '../../utils/utils';
+import { axisBlue, axisOrange, axisGreen, axisPurple, customTooltip } from './chart-station.module.css';
 
 
 export default function ChartStation({ station, selectedAnalytes }) {
-    const [data, setData] = useState({});
+    const [data, setData] = useState(null);
     const [modalVisible, setModalVisible] = useState(false);
     const [loading, setLoading] = useState(true);
 
-    const parseDate = timeParse('%Y-%m-%dT%H:%M:%S');
+    const parseDate = d3.timeParse('%Y-%m-%dT%H:%M:%S');
+    const randomId = useRef(Math.floor((Math.random() * 100000).toString()));
+
+    const responsive = (id) => {
+        // get container + svg aspect ratio
+        const svg = d3.select('#' + id),
+            container = svg.node().parentNode,
+            width = parseInt(svg.style('width')),
+            height = parseInt(svg.style('height')),
+            aspect = width / height;
+        // add viewBox and preserveAspectRatio properties,
+        // and call resize so that svg resizes on inital page load
+        svg.attr('viewBox', '0 0 ' + width + ' ' + height)
+            .attr('perserveAspectRatio', 'xMinYMid')
+            .call(resize);
+        // to register multiple listeners for same event type, 
+        // you need to add namespace, i.e., 'click.foo'
+        // necessary if you call invoke this function for multiple svgs
+        // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+        d3.select(window).on('resize.' + container.id, resize);
+        // get width of container and resize svg to fit it
+        function resize() {
+            const targetWidth = parseInt(container.offsetWidth);
+            svg.attr('width', targetWidth);
+            svg.attr('height', Math.round(targetWidth / aspect));
+        }
+    }
+
+    const drawChart = () => {
+        const chartId = 'chart-' + randomId.current;
+        const margin = { top: 30, right: 40, bottom: 60, left: 40 };
+        const width = 645 + margin.left + margin.right;
+        const height = 180 + margin.top + margin.bottom;
+        const clipPadding = 4;
+
+        const chart = d3.select('#station-chart-container').append('svg')
+            .attr('id', chartId)
+            .attr('className', 'chart')
+            .attr('width', width)
+            .attr('height', height)
+            .call(() => { responsive(chartId); });
+        chart.append('clipPath')
+            .attr('id', 'clip')
+            .append('rect')
+            .attr('x', margin.left - clipPadding)
+            .attr('y', 0)
+            .attr('width', width - margin.left - margin.right + clipPadding * 2)
+            .attr('height', height - margin.bottom +clipPadding);
+        chart.append('clipPath')
+            .attr('id', 'clean-clip')
+            .append('rect')
+            .attr('x', margin.left)
+            .attr('y', 0)
+            .attr('width', width - margin.left - margin.right)
+            .attr('height', height - margin.bottom);
+        // add tooltip
+        const tooltip = d3.select('body').append('div')
+            .attr('id', 'station-chart-tooltip')
+            .attr('class', customTooltip)
+            .style('opacity', 0);
+        // Define scales
+        const analyteKeys = Object.keys(data);
+        let allDates = [];
+        for (const key in analyteKeys) {
+            const obj = data[analyteKeys[key]];
+            const dates = data[analyteKeys[key]].data.map(d => d.SampleDate);
+            allDates = [...allDates, ...dates];
+        }
+        const xExtent = d3.extent(allDates);
+        const xScale = d3.scaleTime()
+            .domain(xExtent)
+            .range([margin.left, width - margin.right]);
+
+        // Define multiple y-axes by first finding the result max and min for each dataset
+        for (const key in analyteKeys) {
+            const analyteName = analyteKeys[key];
+            const results = data[analyteName].data.map(d => d.Result);
+            const yMax = d3.max(results);
+            const yMin = d3.min(results);
+            // store back in original data object
+            data[analyteName]['yMax'] = yMax;
+            data[analyteName]['yMin'] = yMin;
+        }
+        // Create a y-scale and y-axis for each dataset and store in dictionary for later access
+        for (const key in analyteKeys) {
+            const analyteName = analyteKeys[key];
+            data[analyteName]['yScale'] = d3.scaleLinear()
+                .domain([data[analyteName].yMin, data[analyteName].yMax])
+                .range([height - margin.bottom, margin.top]);
+            data[analyteName]['yAxis'] = d3.axisLeft()
+                .scale(data[analyteName]['yScale']);
+        }
+        // Define x-axis
+        const xAxis = d3.axisBottom()
+            .scale(xScale);
+        
+        // Draw x-axis
+        chart.append('g')
+            .attr('transform', 'translate(0,' + (height - margin.bottom) + ')')
+            .call(xAxis);
+        
+        // Draw y-axes
+        for (const key in analyteKeys) {
+            const analyteName = analyteKeys[key];
+            if (key == 0) {
+                chart.append('g')
+                    .attr('class', axisBlue)
+                    .attr('transform', 'translate(' + margin.left + ', 0)')
+                    .call(d3.axisLeft().scale(data[analyteName]['yScale']).ticks(5));
+            } else if (key == 1) {
+                chart.append('g')
+                    .attr('class', axisOrange)
+                    .attr('transform', 'translate(' + (width - margin.right) + ', 0)')
+                    .call(d3.axisRight().scale(data[analyteName]['yScale']).ticks(5));
+            } else if (key == 2) {
+                chart.append('g')
+                    .attr('class', axisGreen)
+                    .attr('transform', 'translate(' + (width - margin.right) + ', 0)')
+                    .call(d3.axisLeft().scale(data[analyteName]['yScale']).ticks(5));
+            } else if (key == 3) {
+                chart.append('g')
+                    .attr('class', axisPurple)
+                    .attr('transform', 'translate(' + margin.left + ', 0)')
+                    .call(d3.axisRight().scale(data[analyteName]['yScale']).ticks(5));
+            }
+        }
+
+        for (const key in analyteKeys) {
+            const analyteName = analyteKeys[key];
+            const yScale = data[analyteName]['yScale'];
+
+            // draw lines
+            const line = d3.line()
+                .x(function(d) { return xScale(d.SampleDate); })
+                .y(function(d) { return yScale(d.Result); })
+                .curve(d3.curveMonotoneX); // apply smoothing to line
+            chart.append('path')
+                .datum(data[analyteName]['data'])
+                .attr('class', 'line')
+                .attr('d', line)
+                .attr('stroke', colorPaletteViz[key])
+                .attr('fill', 'none')
+                .attr('stroke-width', 1);
+
+            // draw points
+            const points = chart.append('g')
+                .attr('clip-path', 'url(#clip)');
+            points.selectAll('.circle')
+                .data(data[analyteName]['data'])
+                .enter().append('circle')
+                .attr('className', 'circle')
+                .attr('r', 4)
+                .attr('cx', (d) => { 
+                    return xScale(d.SampleDate); 
+                })
+                .attr('cy', (d) => { 
+                    return yScale(d.Result); 
+                })
+                .attr('fill', (d) => { return colorPaletteViz[key]; })
+                .attr('stroke', '#fff')
+                .on('mouseover', function(currentEvent, d) {
+                    const formatDate = d3.timeFormat('%b %e, %Y');
+                    return tooltip
+                        .style('opacity', 1)
+                        .style('left', (currentEvent.pageX) + 'px')
+                        .style('top', (currentEvent.pageY - 28) + 'px')
+                        .html('<span style="color: #a6a6a6">' + formatDate(d.SampleDate) + '</span><br>' + d.Analyte + ": " + d.Result + ' ' + d.Unit);
+                })
+                .on('mousemove', function(currentEvent, d) {
+                    return tooltip
+                        .style('opacity', 1)
+                        .style('left', (currentEvent.pageX) + 'px')
+                        .style('top', (currentEvent.pageY - 28) + 'px');
+                })
+                .on('mouseout', () => {
+                    return tooltip.style('opacity', 0);
+                })
+                .merge(points)
+                .attr('cx', (d) => { return xScale(d.SampleDate); })
+                .attr('cy', (d) => { return yScale(d.Result); });
+            points.exit()
+                .remove();
+        }
+    };
+
 
     const handleClick = () => {
         if (modalVisible === false) {
@@ -37,21 +221,28 @@ export default function ChartStation({ station, selectedAnalytes }) {
             }
             Promise.all(promises)
                 .then((results) => {
-                    const obj = {
-                        station: station,
-                        analytes: {}
-                    };
+                    let analyteData = {};
                     for (let i = 0; i < results.length; i++) {
                         const analyte = results[i][0].Analyte;
-                        const data = results[i].map(d => { 
-                            return { x: d.timestamp, y: d.Result };
-                        });
-                        obj.analytes[analyte] = data;
+                        const data = results[i];
+                        analyteData[analyte] = { data: data };
                     }
-                    console.log(obj);
+                    setData(analyteData);
                 });
         }
     }, [modalVisible]);
+
+    useEffect(() => {
+        if (data) {
+            setLoading(false);
+        }
+    }, [data])
+
+    useEffect(() => {
+        if (!loading) {
+            drawChart();
+        }
+    }, [loading]);
 
     const getData = (parameter) => {
         return new Promise((resolve, reject) => {
@@ -64,7 +255,9 @@ export default function ChartStation({ station, selectedAnalytes }) {
                     records.forEach(d => {
                         d.SampleDate = parseDate(d.SampleDate);
                         d.Result = +d.Result;
-                        d.timestamp = d.SampleDate.getTime();
+                        if (parameter === 'pH') {
+                            d.Unit = '';
+                        }
                     });
                     resolve(records);
                 });
@@ -90,7 +283,7 @@ export default function ChartStation({ station, selectedAnalytes }) {
                 >
                     <Header icon='chart bar' content={`Selected parameters for ${station}`} />
                     <Modal.Content>
-                        { loading ? 'Loading...' : 'Loaded' }
+                        { loading ? 'Loading...' : <div id="station-chart-container"></div> }
                     </Modal.Content>
                 </Modal> 
             : '' }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -39,7 +39,7 @@ export const regionNumDict = {
     'San Diego': '9'
 }
 
-export const colorPaletteViz = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'];
+export const colorPaletteViz = ['#1f77b4', '#ff7f0e', '#2ca02c', '#af7aa1', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'];
 
 export const fetchData = (url) => {
     return new Promise((resolve, reject) => {
