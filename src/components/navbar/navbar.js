7871c2848ffc6861a63953ba7774363678b6cf57
Michelle Tang <msmetang@gmail.com>
2021-09-29 19:11:18 -0700

    feat: added template for new homepage

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import LinkDropdown from './link-dropdown';
-import { navBar, navTitle, navMenu } from './navbar.module.css';
+import { navBar, logoWrapper, logo, navTitle, navMenu } from './navbar.module.css';
 
 const Navbar = () => {
 
     return (
         <div className={navBar}>
-            <a href="/"><span className={navTitle}>SWAMP Data Dashboard</span></a>
+            <a href="/"><span className={navTitle}>Surface Water Ambient Monitoring Program</span></a>
+            {/*
+            <div className={logoWrapper}>
+                <a href="/"><span className={navTitle}>Surface Water Ambient Monitoring Program</span><img className={logo} src={`SWAMP_logo_white_small.png`} alt="SWAMP Logo" /></a>
+            </div>
+            */}
             <div className={navMenu}>
                 <ul>
-                    <LinkDropdown>About</LinkDropdown>
+                    <LinkDropdown>Dashboard</LinkDropdown>
+                    <LinkDropdown>Learn</LinkDropdown>
+                    <LinkDropdown>Data</LinkDropdown>
                 </ul>
             </div>
         </div>
