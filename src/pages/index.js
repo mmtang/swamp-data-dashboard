0836f0306d760f53e00adc431b18de319f3d6383
Michelle Tang <msmetang@gmail.com>
2022-01-11 09:20:48 -0800

    fix: image file path fix, build ready

--- src/pages/index.js
+++ src/pages/index.js
@@ -104,7 +104,7 @@ export default function Index() {
           >
               <Modal.Content>
                   <div className={modalContent}>
-                    <img className={swampIcon} src={`/swamp_logo_black_small.png`} alt='SWAMP logo' />
+                    <img className={swampIcon} src={`/swamp-logo-black-small.png`} alt='SWAMP logo' />
                     <p>Welcome to the <strong>SWAMP Data Dashboard</strong>, an interactive tool for exploring and downloading water quality data collected by the <a href="https://www.waterboards.ca.gov/water_issues/programs/swamp/" target="_blank" rel="noopener noreferrer">Surface Water Ambient Monitoring Program</a> (SWAMP).</p>
                     <p><Icon name='exclamation triangle' />This dashboard is in <strong>public beta</strong> and may change at any time without prior notification. All data are provisional and should not be used for any particular purpose other than general reference. Feedback and general inquiries: <a href="mailto:swamp@waterboards.ca.gov">swamp@waterboards.ca.gov</a>.</p>
                     <p>
