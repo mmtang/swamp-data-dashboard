4ab5b1de157e097c0e278f7d212b2f5a9775f683
Michelle Tang <msmetang@gmail.com>
2022-07-25 15:19:55 -0700

    fix: changed download file format from tsv to csv

--- src/components/common/download-data.js
+++ src/components/common/download-data.js
@@ -3,28 +3,40 @@ import { Button, Icon } from 'semantic-ui-react';
 import { timeFormat } from 'd3';
 import { rowButton } from './download-data.module.css';
 
-// This component renders a button. When the button is clicked, the array of JavaScript objects passed to the component is converted to a TSV file and downloaded in the browser.
-// I chose tabs over commas because there are a lot of commas in the station names, and I did not want to add quotes around the string values (which is the common way of dealing with this issue).
+// This component renders a button. When the button is clicked, the array of JavaScript objects passed to the component is converted to a CSV file and downloaded in the browser.
 export default function DownloadData({ children, data, fields, color = null }) {   	
     const [dataLoaded, setDataLoaded] = useState(false);
 
-    const formatDate = timeFormat('%Y-%m-%d %H:%M:%S');
+    const formatDate = timeFormat('%Y-%m-%d %H:%M:%S'); // Date formatting for open data portal
 
     // This function converts an array of JavaScript objects to a tab delimited string.
-    const convertArrayOfObjectsToTSV = (array, fields, columnDelimiter = '\t') => {
+    const convertArrayOfObjectsToCSV = (array, fields, columnDelimiter = ',') => {
         if (array.length > 0) {
             // Initialize empty string variable
             // Convert data records to strings and append them
             let dataString = '';
-            const tsvFields = fields ? fields : Object.keys(array[0]);
-            const header = tsvFields.join(columnDelimiter);
+            const csvFields = fields ? fields : Object.keys(array[0]);
+            const header = csvFields.join(columnDelimiter);
             const rows = array.map(obj => {
-                return tsvFields.map(fieldName => { 
+                return csvFields.map(fieldName => { 
                         if (fieldName === 'SampleDate') {
-                            // Formatting of the date in file has to be done here, trying to do so elsewhere changes the date value of the data points in the chart, which breaks the tooltip. Not sure why.
-                            return formatDate(obj[fieldName]); 
+                            // Formatting of the date in file has to be done here, trying to do so elsewhere changes the date value of the data points in the chart, which breaks the tooltip. Not sure why. 
+                            if (obj[fieldName]) {
+                                return formatDate(obj[fieldName]); 
+                            } else {
+                                return null;
+                            }
                         } else {
-                            return obj[fieldName]; 
+                            // If text string contains a comma, wrap double quotes around text string
+                            if (obj[fieldName]) {
+                                if (obj[fieldName].toString().includes(',')) {
+                                    return `"${obj[fieldName]}"`;
+                                } else {
+                                    return obj[fieldName]; 
+                                }
+                            } else {
+                                return null;
+                            }
                         }
                     })
                     .join(columnDelimiter);
@@ -35,20 +47,20 @@ export default function DownloadData({ children, data, fields, color = null }) {
         }
     }
 
-    const downloadTSV = (array) => {
+    const downloadCSV = (array) => {
         const link = document.createElement('a');
-        let tsv = convertArrayOfObjectsToTSV(array, fields);
-        if (tsv == null) return;
-        const filename = 'SWAMP_Data_Dashboard_' + Date.now() + '.txt';
+        let csv = convertArrayOfObjectsToCSV(array, fields);
+        if (csv == null) return;
+        const filename = 'SWAMP_Data_Dashboard_' + Date.now() + '.csv';
 
         // Two lines below are the old way. Doesn't work anymore. Cuts the file off at around 100 lines.
-        // tsv = `data:text/tsv;charset=utf-8,${tsv}`;
+        // tsv = `data:text/tsv;charset=ut-8,${tsv}`;
         // link.setAttribute('href', encodeURI(tsv)); 
 
         // Two lines below are the new way. Required to export ALL rows in the file.
         // https://stackoverflow.com/questions/24610694/export-html-table-to-csv-in-google-chrome-browser/24611096#24611096
-        var tsvData = new Blob([tsv], { type: 'text/plain' }); 
-        link.setAttribute('href', URL.createObjectURL(tsvData));
+        var csvData = new Blob([csv], { type: 'text/csv;charset=utf-8;' }); 
+        link.setAttribute('href', URL.createObjectURL(csvData));
         link.setAttribute('download', filename);
         document.body.appendChild(link);
         link.click();
@@ -57,7 +69,7 @@ export default function DownloadData({ children, data, fields, color = null }) {
 
     const handleClick = () => {
         if (data.length > 0) {
-            downloadTSV(data);
+            downloadCSV(data);
         } else {
             console.error('Empty data array');
         }
