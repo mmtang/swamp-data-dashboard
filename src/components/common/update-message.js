6865d96973f284e258fc163e4ebf52e1447c49ae
Michelle Tang <msmetang@gmail.com>
2023-01-11 11:11:52 -0800

    fix: updated resource that is being used for the last refresh message;

--- src/components/common/update-message.js
+++ src/components/common/update-message.js
@@ -19,9 +19,9 @@ const UpdateMessage = () => {
 
         const getDate = () => {
             // Fetch metadata of resource and extract the last modified value
-            // This URL points to the SWAMP Stations - Summary dataset on the portal. Any of the other SWAMP datasets could be used with the same result.
-            // https://data.ca.gov/dataset/surface-water-ambient-monitoring-program/resource/555ee3bf-891f-4ac4-a1fc-c8855cf70e7e
-            let url = 'https://data.ca.gov/api/3/action/resource_show?id=555ee3bf-891f-4ac4-a1fc-c8855cf70e7e';
+            // This URL points to the SWAMP Stations dataset on the portal. Any of the other SWAMP datasets could be used with the same result.
+            // https://data.ca.gov/dataset/surface-water-ambient-monitoring-program/resource/df69fdd7-1475-4e57-9385-bb1514f0291e
+            let url = 'https://data.ca.gov/api/3/action/resource_show?id=df69fdd7-1475-4e57-9385-bb1514f0291e';
             fetchData(url)
             .then(json => json.result)
             .then(result => {
